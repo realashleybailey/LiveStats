a1f09615e28ea88238713f2a15821e4e29d48f9d
Ashley <admin@ashleybailey.me>
2023-03-15 05:35:39 +0000

    update

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,44 @@
 import { createServer } from 'http';
-import { fileURLToPath } from 'url';
 import { Tail } from 'tail';
 import { Server } from 'socket.io';
 import path from 'path';
+import fs from 'fs';
 
 import app from './http-server.js';
 
-// Get the path to the log file 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Create a HTTP server
 const server = createServer(app);
 const io = new Server(server);
 
 // Path to the log file
-// const filePath = path.join(__dirname, '..', 'log_file.log');
-const filePath = "/private/var/log/com.apple.xpc.launchd/launchd.log";
-
 io.on('connection', (socket) => {
+
     console.log('A user connected');
 
+    const directory = '/var/log/jellyfin';
+    const files = fs.readdirSync(directory);
+
+    let newestFile = null;
+    let newestTimestamp = 0;
+
+    files.forEach(file => {
+        if (file.startsWith('jellyfin') && file.endsWith('.log')) {
+            const filePath = path.join(directory, file);
+            const stats = fs.statSync(filePath);
+            const timestamp = stats.mtimeMs;
+
+            if (timestamp > newestTimestamp) {
+                newestFile = filePath;
+                newestTimestamp = timestamp;
+            }
+        }
+    });
+
+    console.log(`Using log file: ${newestFile}`);
+
     // Listen to filePath using tail
-    const tail = new Tail(filePath);
+    const tail = new Tail(newestFile);
 
     // Send data to the client
     tail.on('line', (data) => {
@@ -30,5 +47,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(9898, () => {
-    console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 9898');
 });
